232d64f85a47bf0225e4de42034f3d1941d8289f
Debasish Panda <pandadebasish57@gmail.com>
2021-06-13 16:43:48 +0530

    path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,10 @@ const routes: Routes = [
     component:DashboardComponent
   },
   {
+    path:"",
+    component:DashboardComponent
+  },
+  {
     path:"productlist",
     component:ProductlistComponent
   },
